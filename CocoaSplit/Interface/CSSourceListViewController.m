41f1dde7e3d8dbe059ef5a67f2c8029fa65c8104
Zakk <zakk@rsdio.com>
2018-01-14 18:35:01 -0500

    Operations done via the source list outline view now have undo/redo

--- CocoaSplit/Interface/CSSourceListViewController.m
+++ CocoaSplit/Interface/CSSourceListViewController.m
@@ -179,12 +179,102 @@
         }
         
         [sourceLayout addSource:source];
+        NSUndoManager *undoManager = self.view.window.undoManager;
+        [[undoManager prepareWithInvocationTarget:self] deleteSourcesByUUID:@[source.uuid]];
     }
 }
+
+-(void)attachByUUUID:(NSString *)srcUUID toUUID:(NSString *)toUUID
+{
+    SourceLayout *sourceLayout = self.sourceLayoutController.content;
+    if (!srcUUID || !toUUID) //??
+    {
+        return;
+    }
+    
+    InputSource *src = (InputSource *)[sourceLayout inputForUUID:srcUUID];
+    InputSource *parent = (InputSource *)[sourceLayout inputForUUID:toUUID];
+    if (src && parent)
+    {
+        [parent attachInput:src];
+        [[self.view.window.undoManager prepareWithInvocationTarget:self] detachSourcesByUUID:@[src.uuid]];
+    }
+}
+
+
+
+-(void)detachSourcesByUUID:(NSArray *)uuids
+{
+    SourceLayout *sourceLayout = self.sourceLayoutController.content;
+    
+    for (NSString *uuid in uuids)
+    {
+        InputSource *src = (InputSource *)[sourceLayout inputForUUID:uuid];
+        InputSource *parent = src.parentInput;
+        if (parent)
+        {
+            [parent detachInput:src];
+            [[self.view.window.undoManager prepareWithInvocationTarget:self] attachByUUUID:src.uuid toUUID:parent.uuid];
+        }
+    }
+}
+
+
+-(void)restoreDepths:(NSDictionary *)depthMap
+{
+    SourceLayout *sourceLayout = self.sourceLayoutController.content;
+    
+    NSUndoManager *undoManager = self.view.window.undoManager;
+    
+    NSMutableDictionary *currentDepths = [NSMutableDictionary dictionary];
+    
+    for (NSObject <CSInputSourceProtocol> *src in sourceLayout.sourceList)
+    {
+        [currentDepths setObject:[NSNumber numberWithFloat:src.depth] forKey:src.uuid];
+    }
+    
+    for (NSString *uuid in depthMap)
+    {
+        NSObject <CSInputSourceProtocol> *src = [sourceLayout inputForUUID:uuid];
+        NSNumber *depth = depthMap[uuid];
+        float fDepth = depth.floatValue;
+        src.depth = fDepth;
+    }
+    
+    [sourceLayout generateTopLevelSourceList];
+    [[undoManager prepareWithInvocationTarget:self] restoreDepths:currentDepths];
+}
+
+
+-(void)undoDepthForUUID:(NSString *)uuid oldDepth:(float)oldDepth
+{
+    SourceLayout *sourceLayout = self.sourceLayoutController.content;
+    
+    NSUndoManager *undoManager = self.view.window.undoManager;
+    
+    NSObject <CSInputSourceProtocol> *src = [sourceLayout inputForUUID:uuid];
+    if (!src)
+    {
+        return;
+    }
+    
+    float cDepth = src.depth;
+    src.depth = oldDepth;
+    if (cDepth != oldDepth)
+    {
+        [[undoManager prepareWithInvocationTarget:self] undoDepthForUUID:uuid oldDepth:cDepth];
+    }
+    
+    [sourceLayout generateTopLevelSourceList];
+}
+
+
 -(BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id<NSDraggingInfo>)info item:(id)item childIndex:(NSInteger)index
 {
     
+    NSUndoManager *undoManager = self.view.window.undoManager;
     
+    [undoManager beginUndoGrouping];
     NSPasteboard *pb = [info draggingPasteboard];
     SourceLayout *sourceLayout = self.sourceLayoutController.content;
     
@@ -210,6 +300,7 @@
                 
             }
         }
+        [undoManager endUndoGrouping];
         return YES;
     }
     
@@ -217,6 +308,13 @@
     
     NSTreeNode *parentNode = (NSTreeNode *)item;
     InputSource *parentSource = nil;
+    
+    NSMutableDictionary *currentDepths = [NSMutableDictionary dictionary];
+    
+    for (NSObject <CSInputSourceProtocol> *src in sourceLayout.sourceList)
+    {
+        [currentDepths setObject:[NSNumber numberWithFloat:src.depth] forKey:src.uuid];
+    }
     
     
     NSIndexPath *droppedIdxPath = nil;
@@ -276,17 +374,20 @@
                 }
                 
                 [iSrc.parentInput detachInput:iSrc];
+                [[undoManager prepareWithInvocationTarget:self] attachByUUUID:iSrc.uuid toUUID:iSrc.parentInput.uuid];
                 
             }
             if (parentSource)
             {
                 [parentSource attachInput:iSrc];
+                [[undoManager prepareWithInvocationTarget:self] detachSourcesByUUID:@[iSrc.uuid]];
             }
             iSrc.depth = newDepth++;
         }
         
-        
+        [[undoManager prepareWithInvocationTarget:self] restoreDepths:currentDepths];
         [sourceLayout generateTopLevelSourceList];
+        [undoManager endUndoGrouping];
         return YES;
     }
     
@@ -315,6 +416,7 @@
         {
             
             [self addInputSourceWithInput:itemSrc];
+            
             if (itemSrc.isVideo)
             {
                 InputSource *lsrc = (InputSource *)itemSrc;
@@ -437,18 +539,109 @@
 }
 
 
+-(void)deleteSourcesByUUID:(NSArray *)uuids
+{
+    
+    NSUndoManager *undoManager = self.view.window.undoManager;
+    
+    NSMutableArray *undoMap = [NSMutableArray array];
+    
+    for (NSString *uuid in uuids)
+    {
+        SourceLayout *srcLayout = self.sourceLayoutController.content;
+        
+        NSObject <CSInputSourceProtocol> *obj = [srcLayout inputForUUID:uuid];
+        if (!obj)
+        {
+            continue;
+        }
+        [srcLayout deleteSource:obj];
+        NSDictionary *undoEntry = nil;
+        if (obj.isVideo)
+        {
+            InputSource *vsrc = (InputSource *)obj;
+            if (vsrc.parentInput)
+            {
+                undoEntry = @{@"parentUUID": vsrc.parentInput.uuid, @"source": obj};
+            }
+        }
+        
+        if (!undoEntry)
+        {
+            undoEntry = @{@"source": obj};
+        }
+        
+        [undoMap addObject:undoEntry];
+    }
+    
+    NSData *undoData = [NSKeyedArchiver archivedDataWithRootObject:undoMap];
+    [[undoManager prepareWithInvocationTarget:self]  undoDeleteSources:undoData];
+}
+
+
+-(void)undoDeleteSources:(NSData *)withData
+{
+    NSArray *undoList = [NSKeyedUnarchiver unarchiveObjectWithData:withData];
+    NSMutableArray *uuids = [NSMutableArray array];
+    
+    SourceLayout *sourceLayout = self.sourceLayoutController.content;
+    for (NSDictionary *undoMap in undoList)
+    {
+        NSObject <CSInputSourceProtocol> *obj = undoMap[@"source"];
+        [sourceLayout addSource:obj];
+        [uuids addObject:obj.uuid];
+        NSString *parentUUID = [undoMap objectForKey:@"parentUUID"];
+        if (obj.isVideo && parentUUID)
+        {
+            InputSource *vParent = (InputSource *)[sourceLayout inputForUUID:parentUUID];
+            [vParent attachInput:(InputSource *)obj];
+        }
+    }
+    NSUndoManager *undoManager = self.view.window.undoManager;
+    [[undoManager prepareWithInvocationTarget:self] deleteSourcesByUUID:uuids];
+    
+}
+
+
+
+
+
 -(IBAction)sourceDeleteClicked:(NSButton *)sender
 {
     
     NSArray *selectedSources = self.sourceTreeController.selectedObjects;
     
+    NSUndoManager *undoManager = self.view.window.undoManager;
+
+    NSMutableArray *undoMap = [NSMutableArray array];
+    
     for (NSObject<CSInputSourceProtocol> *obj in selectedSources)
     {
         SourceLayout *srcLayout = self.sourceLayoutController.content;
+        
         [srcLayout deleteSource:obj];
+        NSDictionary *undoEntry = nil;
+        if (obj.isVideo)
+        {
+            InputSource *vsrc = (InputSource *)obj;
+            if (vsrc.parentInput)
+            {
+                undoEntry = @{@"parentUUID": vsrc.parentInput.uuid, @"source": obj};
+            }
+        }
+        
+        if (!undoEntry)
+        {
+            undoEntry = @{@"source": obj};
+        }
+        
+        [undoMap addObject:undoEntry];
     }
     
+    NSData *undoData = [NSKeyedArchiver archivedDataWithRootObject:undoMap];
+    [[undoManager prepareWithInvocationTarget:self]  undoDeleteSources:undoData];
 }
+
 
 -(IBAction)sourceConfigClicked:(NSButton *)sender
 {
